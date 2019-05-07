1a31e479fcba79411ecff45344aa635a8065b49a
shaayma <48011951+shaayma@users.noreply.github.com>
2019-05-06 19:41:25 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,34 +1,74 @@
-#include "defs.h"
-#include "jeu.h"
-#include <SDL/SDL.h>
+#include <stdio.h>
+#include "SDL/SDL.h"
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include<SDL/SDL_ttf.h>
+#include"partage.h"
+int main() {
 
 
-int main ( int argc, char** argv )
+SDL_Surface *screen=NULL,*player1,*player2,*background1,*background2;
+SDL_Rect posback1,posback2,posplayer1,posplayer2;
+SDL_Event event;
+  int game =1,directionSDL1,directionSDL2;
+  background1=IMG_Load("background1.png");
+  background2=IMG_Load("background2.png");
+  player1=IMG_Load("player1.png");
+  player2=IMG_Load("player2.png");
+  SDL_Init( SDL_INIT_EVERYTHING );
+  screen = SDL_SetVideoMode(1200, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+initposition(&posback1,&posback2,&posplayer1,&posplayer2);
+//initimage(background1,background2 ,player1,player2);
+SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY,SDL_DEFAULT_REPEAT_INTERVAL);
+
+while (game)
 {
+  //input from SDL
+  while(SDL_PollEvent(&event)){
+        switch (event.type)
+        {
+        // exit if the window is closed
+        case SDL_QUIT:
+            game = 0;
+            break;
+        case SDL_KEYDOWN:
+        {
 
-	SDL_Surface *screen;
-	// initialize SDL video
-	if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
-		printf( "Unable to init SDL: %s\n", SDL_GetError() );
-		return 1;
-	}
+            if (event.key.keysym.sym == SDLK_d)//perso 1
+              directionSDL1 = 1;
 
-	// make sure SDL cleans up before exit
-	atexit(SDL_Quit);
+            if (event.key.keysym.sym == SDLK_q)//perso 1
+              directionSDL1 = 2;
+          if (event.key.keysym.sym == SDLK_m)//perso 2
+          directionSDL2 = 1;
+          if (event.key.keysym.sym == SDLK_l)//perso 2
+          directionSDL2 = 2;
+        }break;
+        break;
+        case SDL_KEYUP:
+          directionSDL1=0;
+          directionSDL2=0;
+        break;
 
-	// create a new window
-	screen = SDL_SetVideoMode(SCREEN_W, SCREEN_H, 32,
-	                          SDL_HWSURFACE|SDL_DOUBLEBUF | SDL_SRCALPHA);
-	if ( !screen ) {
-		printf("Unable to set 700x560 video: %s\n", SDL_GetError());
-		return 1;
-	}
-
-
-	jouer(screen);
+      }}
+movementplayer(&directionSDL1,&directionSDL2,&posplayer1,&posplayer2);
 
 
-	// all is well ;)
-	printf("Exited cleanly\n");
-	return 0;
+SDL_BlitSurface(background1,NULL,screen,&posback1);
+SDL_BlitSurface(background2,NULL,screen,&posback2);
+SDL_BlitSurface(player1,NULL,screen,&posplayer1);
+SDL_BlitSurface(player2,NULL,screen,&posplayer2);
+SDL_Flip(screen);
+}
+
+
+
+
+
+SDL_FreeSurface(background1);
+SDL_FreeSurface(background2);
+SDL_FreeSurface(player1);
+SDL_FreeSurface(player2);
+
+
 }
